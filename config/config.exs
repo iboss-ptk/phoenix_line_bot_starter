c2e31514d69debc7b9d1dde60a6c8cba14f3a4d7
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2018-08-13 11:35:37 +0700

    Read secrets from env variables

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,11 @@ config :line_bot_starter, LineBotStarterWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "3ayF2dB2VqsAxB3txOQUrPkxqPBjB4XbRlsRp7gwa3GkaTmsG0iX4FvO23wxIhR0",
   render_errors: [view: LineBotStarterWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: LineBotStarter.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: LineBotStarter.PubSub, adapter: Phoenix.PubSub.PG2]
+
+config :line_bot_starter, LineBotStarterWeb.LineAuthentication,
+  channel_secret: System.get_env("LINE_CHANNEL_SECRET"),
+  channel_access_token: System.get_env("LINE_CHANNEL_ACCESS_TOKEN")
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -20,4 +23,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
