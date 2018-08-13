73662efd123683a87e7b6b7667024667fc334198
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2018-08-13 11:56:55 +0700

    Reply user with some stupid response

--- lib/line_bot_starter_web/controllers/bot_controller.ex
+++ lib/line_bot_starter_web/controllers/bot_controller.ex
@@ -1,7 +1,27 @@
 defmodule LineBotStarterWeb.BotController do
   use LineBotStarterWeb, :controller
 
-  def webhook(conn, _params) do
+  def webhook(conn, %{"events" => [%{"replyToken" => replyToken}]}) do
+    line_auth = Application.get_env(:line_bot_starter, LineBotStarterWeb.LineAuthentication)
+
+    HTTPoison.post!(
+      "https://api.line.me/v2/bot/message/reply",
+      Poison.encode!(%{
+        "replyToken" => replyToken,
+        "messages" => [
+          %{
+            "type" => "text",
+            "text" => Enum.random(["อือ", "555", "ค่ะ"])
+          }
+        ]
+      }),
+      [
+        {"Content-Type", "application/json"},
+        {"Authorization", "Bearer #{line_auth[:channel_access_token]}"}
+        |> IO.inspect(label: "acc")
+      ]
+    )
+
     conn |> send_resp(200, "OK")
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule LineBotStarter.Mixfile do
       app: :line_bot_starter,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -25,7 +25,7 @@ defmodule LineBotStarter.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -35,7 +35,8 @@ defmodule LineBotStarter.Mixfile do
       {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:httpoison, "~> 1.2"}
     ]
   end
 end
